8e896567d850e8ae482de546e9ceab914612fccd
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2024-04-06 23:29:05 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+};
+let userInput = await inquirer.prompt([{
+        message: "ENTER FROM CURRENCY",
+        name: "from",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        message: "ENTER TO CURRENCY",
+        name: "to",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: "amount",
+        message: "ENTER YOUR AMOUNT",
+        type: "number"
+    }
+]);
+let fromAmount = currency[userInput.from];
+let toAmount = currency[userInput.to];
+let amount = userInput.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
+// let data:any={
+//     name:'vikram',
+//     age:22,
+//     rollNumber:'f2321',
+//     address:'karachi',
+// };
+// let property = 'age'
+// console.log(data[property]);
